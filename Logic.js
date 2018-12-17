cf0370606e8787464eb99dbb9c5212b17cf3e390
Abhay Kumar <AK54387@zender.local>
2018-12-17 16:01:03 +0530

    added random rects and effect on ball touch

--- Logic.js
+++ Logic.js
@@ -7,17 +7,23 @@ var ballY = 50;
 var mouseX = 0;
 var paddleX = 0;
 var score = 0;
+var dx = 5;
+var dy = 2;
 const BALL_RADIUS = 7;
 const PADDLE_HEIGHT = 15;
 const PADDLE_WIDTH = 150;
 const MIN_HEIGHT_TO_CHECK = PADDLE_HEIGHT + 5;
 var flagToCheck = true;
-var ballColor='green';
+var ballColor = 'green';
+const OBSTACLE_HEIGHT = 40;
+const OBSTACLE_WIDTH = 50;
+var obstacleMap = [];
+var  taggedcolor = '#';
 
 
 window.onload = function (e) {
     initializeGame();
-drawRandomRectangle();
+    drawRandomRectangle();
     // context.beginPath();
 
     canvas.addEventListener('mousemove', function (evt) {
@@ -29,39 +35,39 @@ drawRandomRectangle();
 
 
     //Move Ball
-   var intrvl= setInterval(
+    var intrvl = setInterval(
         function () {
 
 
 
-            document.getElementById('score').innerHTML=score;
-            ballX += 5;
-            ballY += 2;
+            document.getElementById('score').innerHTML = score;
+            ballX += dx;
+            ballY += dy;
 
 
             if (Math.abs(ballY) > canvas.height - MIN_HEIGHT_TO_CHECK) {
                 if (flagToCheck) {
                     if (Math.abs(ballX) > paddleX && Math.abs(ballX) < paddleX + PADDLE_WIDTH) {
                         score++;
-                        ballColor=getRandomColor();
+                        setRandomColor();
+                        ballColor = taggedcolor;
                         flagToCheck = false;
                     }
                     else {
                         alert('lost');
                         context.clearRect(0, 0, canvas.width, canvas.height);
-                       //initializeGame();
-                       clearInterval(intrvl);
-                       
+                        //initializeGame();
+                        clearInterval(intrvl);
+
                     }
 
-                    
+
                 }
             }
-            else
-                    {
-                        flagToCheck = true;
+            else {
+                flagToCheck = true;
 
-                    }
+            }
 
             //Craete Ball
             if (ballX >= 0) {
@@ -111,25 +117,29 @@ function getMousePos(canvas, evt) {
 //Move the Ball
 function moveBall(x, y) {
 
+
+   
     //Create Paddle
-    context.globalAlpha=0.3;
+    context.globalAlpha = 0.3;
     context.fillStyle = 'white';
     context.fillRect(0, 0, 700, 700);
-    context.globalAlpha=1;
+     
+    context.globalAlpha = 1;
 
 
-  
+
 
     context.beginPath();
-    context.fillStyle=ballColor;
+    context.fillStyle = ballColor;
     context.arc(x, y, BALL_RADIUS, 0, Math.PI * 2, false);
- context.fill();
+    context.fill();
 
-      context.fillStyle = 'black';
+    context.fillStyle = 'black';
     context.fillRect(paddleX, canvas.height - PADDLE_HEIGHT, PADDLE_WIDTH, PADDLE_HEIGHT);
 
-drawRandomRectangle();
-   
+   drawRandomRectangle();
+   collisionCheck(x, y);
+
 
 }
 
@@ -141,39 +151,58 @@ function movePaddle(x) {
 }
 
 
-function initializeGame()
-{
-     canvas = document.getElementById("canvasGame");
+function initializeGame() {
+    canvas = document.getElementById("canvasGame");
     context = canvas.getContext('2d');
 }
 
-function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+function setRandomColor() {
+    var letters = '0123456789ABCDEF';
+  taggedcolor='#';
+    for (var i = 0; i < 6; i++) {
+        taggedcolor += letters[Math.floor(Math.random() * 16)];
+    }
+    
+}
+
+function collisionCheck(x, y) {
+    obstacleMap.forEach(function (item) {
+        if (x > item.ox && x < item.ox + OBSTACLE_WIDTH) {
+            if (y > item.oy && y < item.oy + OBSTACLE_HEIGHT) {
+                drawObstacle(item.ox,item.oy,taggedcolor);
+            }
+        }
+    })
+
 }
 
 
 
+function drawRandomRectangle() {
 
 
-function drawRandomRectangle()
-{
 
-for(var i=1;i<10;i++)
-{
-     context.beginPath();
-    context.rect(i+10, i+100, 100, 100);
-    context.fillStyle = 'black';
-    context.closePath();
-    context.fill();
+    for (var i = 1; i <= 5; i++) {
+        for (var j = 1; j <= 5; j++) {
 
-    //    context.fillStyle = 'black';
-    // context.fillRect(i, 200, PADDLE_WIDTH, PADDLE_HEIGHT);
+            var obstacle = { ox: j * 100, oy: i * 70 };
+            obstacleMap.push(obstacle);
+          drawObstacle(j*100,i*70,'black');
+        }
+
+
+        //    context.fillStyle = 'black';
+        // context.fillRect(i, 200, PADDLE_WIDTH, PADDLE_HEIGHT);
+
+    }
 
 }
 
+function drawObstacle(x,y,color)
+{
+      context.beginPath();
+            context.rect(x,y, OBSTACLE_WIDTH, OBSTACLE_HEIGHT);
+            context.fillStyle = color;
+            context.closePath();
+            context.fill();
 }
