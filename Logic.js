596a7ce2e74fcdc89220b373e5a361551fb06dfc
Abhay Kumar <AK54387@zender.local>
2018-12-21 17:37:45 +0530

    Payline horizon algo

--- Logic.js
+++ Logic.js
@@ -1,6 +1,6 @@
 
 var canvas = null;
-var canvasSlot=null;
+var canvasSlot = null;
 var context = null;
 var paddle = null;
 var ballX = 50;
@@ -14,17 +14,17 @@ const BALL_RADIUS = 7;
 const PADDLE_HEIGHT = 15;
 const PADDLE_WIDTH = 150;
 const MIN_HEIGHT_TO_CHECK = PADDLE_HEIGHT + 5;
-const PADDLE_COLOR='white';
+const PADDLE_COLOR = '#ff8d00';
 var flagToCheck = true;
 var ballColor = '';
 const OBSTACLE_HEIGHT = 50;
 const OBSTACLE_WIDTH = 50;
 var obstacleMap = [];
-var  taggedcolor = '#';
+var taggedcolor = '#';
 var paddelStrikeMusic;
 var bgMusic;
-var cardMap=[1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5];
-var slotTable=[];
+var cardMap = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5];
+var slotTable = [];
 
 window.onload = function (e) {
     initializeGame();
@@ -42,12 +42,11 @@ window.onload = function (e) {
     //Move Ball
     var intrvl = setInterval(
         function () {
- bgMusic.play();
- if(bgMusic.sound.currentTime >=27)
- {
-     bgMusic.reset();
-     //bgMusic.play();
- }
+            bgMusic.play();
+            if (bgMusic.sound.currentTime >= 27) {
+                bgMusic.reset();
+                //bgMusic.play();
+            }
 
 
             document.getElementById('score').innerHTML = score;
@@ -62,20 +61,24 @@ window.onload = function (e) {
                         setRandomColor();
                         ballColor = taggedcolor;
                         paddelStrikeMusic.play();
-                       
-                        
-                         
-                         obstacleMap.forEach(function(item)
-                         {
-                             if(item.show)
-                             {
-                                 slotTable.push(item.card);
-                             }
-                         });
-                         paintSlot();
-                         drawRandomRectangle();
+
+
+
+                        obstacleMap.forEach(function (item) {
+                            if (slotTable.length <= 25) {
+                                if (item.show) {
+                                    slotTable.push(item.card);
+                                }
+                            }
+                            else {
+                                clearInterval(intrvl);
+                                checkForPayline();
+                            }
+                        });
+                        paintSlot();
+                        drawRandomRectangle();
                         flagToCheck = false;
-                        console.log(slotTable);
+
                     }
                     else {
                         alert('lost');
@@ -142,12 +145,12 @@ function getMousePos(canvas, evt) {
 function moveBall(x, y) {
 
 
-  
- 
-   
+
+
+
     //Create Paddle
     context.globalAlpha = 0.3;
-  
+
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.globalAlpha = 1;
 
@@ -162,19 +165,20 @@ function moveBall(x, y) {
     context.fillStyle = PADDLE_COLOR;
     context.fillRect(paddleX, canvas.height - PADDLE_HEIGHT, PADDLE_WIDTH, PADDLE_HEIGHT);
 
-  //drawRandomRectangle();
-  drawObstacle();
-   
-   collisionCheck(x, y);
+
+    drawObstacle();
+
+    collisionCheck(x, y);
 
 
 
 }
 
 function movePaddle(x) {
+
+
     context.fillStyle = PADDLE_COLOR;
     context.fillRect(x, canvas.height - PADDLE_HEIGHT, PADDLE_WIDTH, PADDLE_HEIGHT);
-
 
 }
 
@@ -186,33 +190,32 @@ function initializeGame() {
     canvasSlot = document.getElementById("canvasSlot");
     contextSlot = canvasSlot.getContext('2d');
     setRandomColor();
-    ballColor=taggedcolor;
-    paddelStrikeMusic=new sound("assets/bounce.mp3",false);
-    bgMusic=new sound("assets/bg.mp3",true);
-  
-  
+    ballColor = taggedcolor;
+    paddelStrikeMusic = new sound("assets/bounce.mp3", false);
+    bgMusic = new sound("assets/bg.mp3", true);
+
+
 }
 
 
 function setRandomColor() {
     var letters = '0123456789ABCDEF';
-  taggedcolor='#';
+    taggedcolor = '#';
     for (var i = 0; i < 6; i++) {
         taggedcolor += letters[Math.floor(Math.random() * 16)];
     }
-    
+
 }
 
 function collisionCheck(x, y) {
-   
-   
+
+
     obstacleMap.forEach(function (item) {
         if (x > item.ox && x < item.ox + OBSTACLE_WIDTH) {
             if (y > item.oy && y < item.oy + OBSTACLE_HEIGHT) {
-item.show=true;
-               // drawImage(item.ox,item.oy,item.card,item.show);
-               
-                
+                item.show = true;
+
+
             }
         }
     })
@@ -229,119 +232,141 @@ function shuffleArray(array) {
 }
 
 function drawRandomRectangle() {
-obstacleMap=[];
-shuffleArray(cardMap);
+    obstacleMap = [];
+    shuffleArray(cardMap);
 
-var count=0;
+    var count = 0;
     for (var i = 1; i <= 5; i++) {
         for (var j = 1; j <= 5; j++) {
- 
 
-            var obstacle = { ox: j * 100, oy: i * 70 , card:cardMap[count], show:false };
+
+            var obstacle = { ox: j * 100, oy: i * 70, card: cardMap[count], show: false };
             obstacleMap.push(obstacle);
-         //drawObstacle(j*100,i*70,'white');
-        // drawImage();
-        
-          count++;
+
+            count++;
         }
 
 
-        //    context.fillStyle = 'black';
-        // context.fillRect(i, 200, PADDLE_WIDTH, PADDLE_HEIGHT);
+
 
     }
 
 }
 
-function drawObstacle()
-{
+function drawObstacle() {
 
-    
 
-               drawImage();
-             
+
+    drawImage();
+
 }
 
 
-function sound(src,flag) {
+function sound(src, flag) {
     this.sound = document.createElement("audio");
     this.sound.src = src;
     this.sound.setAttribute("preload", "auto");
     this.sound.setAttribute("controls", "none");
-    if(flag)
-    {
-    this.sound.setAttribute("loop",flag);
+    if (flag) {
+        this.sound.setAttribute("loop", flag);
     }
     this.sound.style.display = "none";
     document.body.appendChild(this.sound);
-    this.play = function(){
+    this.play = function () {
         this.sound.play();
     }
-    this.stop = function(){
+    this.stop = function () {
         this.sound.pause();
-    }    
-    this.reset=function()
-    {
-        this.sound.currentTime=0;
+    }
+    this.reset = function () {
+        this.sound.currentTime = 0;
         this.sound.play();
     }
-} 
+}
 
-function paintSlot()
-{
-   var n=10;var m=0;
-    for(var i=1;i<=slotTable.length;i++)
-    {
-       
-        
-        var img=new Image();
-        img.src="assets/"+slotTable[i-1]+".png";
-        contextSlot.drawImage(img, 25+ 15*m+(m*OBSTACLE_WIDTH), n,OBSTACLE_WIDTH,OBSTACLE_HEIGHT);
+function paintSlot() {
+    var n = 0; var m = 0; var y = 15;
+    for (var i = 1; i <= slotTable.length; i++) {
+
+
+        var img = new Image();
+        img.src = "assets/" + slotTable[i - 1] + ".png";
+        contextSlot.drawImage(img, 25 + 15 * m + (m * OBSTACLE_WIDTH), n + y, OBSTACLE_WIDTH, OBSTACLE_HEIGHT);
         m++;
-        if(i%5==0)
-        {
-            n+=OBSTACLE_HEIGHT;
-            m=0;
+
+        if (i % 5 == 0) {
+            n += OBSTACLE_HEIGHT;
+            m = 0;
+            y += 15;
         }
     }
 }
 
-function drawImage()
-{
-  
+function drawImage() {
+
     obstacleMap.forEach(function (item) {
-      
-        //   context.beginPath();
-        //             context.rect(item.ox,item.oy, OBSTACLE_WIDTH, OBSTACLE_HEIGHT);
-        //             context.fillStyle = "white";
-        //             context.closePath();
-        //             context.strokeStyle='grey';
-        //             context.stroke();
-        //             context.fill();
-        
-        //var imageObj = new Image();
-        if(item.show){
+
+
+        if (item.show) {
             var imageObj = new Image();
-       imageObj.src = "assets/"+item.card+".png";
-               
-        context.drawImage(imageObj, item.ox, item.oy,OBSTACLE_WIDTH,OBSTACLE_HEIGHT);
+            imageObj.src = "assets/" + item.card + ".png";
+
+            context.drawImage(imageObj, item.ox, item.oy, OBSTACLE_WIDTH, OBSTACLE_HEIGHT);
         }
-        else
-        {
+        else {
             var imageObj1 = new Image();
             imageObj1.src = "assets/0.png";
-               
-        context.drawImage(imageObj1, item.ox, item.oy,OBSTACLE_WIDTH,OBSTACLE_HEIGHT);
+
+            context.drawImage(imageObj1, item.ox, item.oy, OBSTACLE_WIDTH, OBSTACLE_HEIGHT);
         }
-                      //  drawImage(item.ox,item.oy,'gift');
-                      // 
-                        
-                   
-            
-        
-             });
-      
-     
-                
-            
+
+
+
+
+    });
+
+
+
+
 }
+
+
+function checkForPayline() {
+    var PayLine = [];
+    var itemObj = {};
+    var maxCount=0;
+    var count = 0;
+    var n=5;
+    itemObj = { item: slotTable[0], itemCount: 1 };
+    for (var i = 0; i < 25; i++) {
+ count++;
+       if((i+1)%5==0)
+       {
+           n+=5;
+           PayLine.push(itemObj);
+           itemObj = { item: slotTable[i], itemCount: 1 };
+       }
+        maxCount=1;
+        for(var j=i+1;j<n;j++)
+        {
+            if(itemObj.item==slotTable[j])
+            {
+                maxCount++;
+            }
+            if(maxCount>itemObj.itemCount)
+            {
+                itemObj.itemCount=maxCount;
+                itemObj.item=slotTable[j];
+    
+            }
+
+        }
+       
+        
+      
+    }
+
+    console.log(PayLine);
+}
+
+
